7439c0722396bb18012e531523c6f3ca600b0b2f
Andy Chen <andychen5671@gmail.com>
2026-04-30 22:26:23 -0400

    Updates

--- _next/static/gtwxCGTt8RSN2RItZZhRw/_buildManifest.js
+++ _next/static/gtwxCGTt8RSN2RItZZhRw/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/veryable-frontend-challenge//_next/:path+",
+        "destination": "/veryable-frontend-challenge/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
